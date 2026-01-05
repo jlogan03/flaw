6702687f3c3d2aa2cfd645bdcbde813085014462
James Logan <jlogan03@gmail.com>
2026-01-04 20:09:58 -0500

    Update deps & benchmarks (#22)
    ## 0.6.1 - 2026-01-04
    
    ### Changed
    
    * Update dep versions
    * Cascade `fma` feature to `interpn`
    * Benchmark more filter configurations

--- flaw/benches/bench.rs
+++ flaw/benches/bench.rs
@@ -1,5 +1,5 @@
 use criterion::{Criterion, criterion_group, criterion_main};
-use flaw::sos;
+use flaw::{butter2, polynomial_fractional_delay, sos};
 use std::hint::black_box;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
@@ -7,7 +7,52 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
     let mut butter4_f64 = sos::butter4::<f64>(cutoff_ratio).unwrap();
     c.bench_function("sos butter4 f64", |b| {
-        b.iter(|| black_box(butter4_f64.update(1.0)))
+        let input = black_box(1.0);
+        b.iter(|| black_box(butter4_f64.update(input)))
+    });
+
+    let mut butter2_f32 = butter2(cutoff_ratio).unwrap();
+    c.bench_function("statespace butter2 f32", |b| {
+        let input = black_box(1.0);
+        b.iter(|| black_box(butter2_f32.update(input)))
+    });
+
+    let mut butter2_bank = [butter2_f32; 20];
+    c.bench_function("statespace butter2 f32 bank 20x", |b| {
+        let input = black_box([1.0; 20]);
+        b.iter(|| {
+            black_box(
+                butter2_bank
+                    .iter_mut()
+                    .zip(input.iter())
+                    .for_each(|(f, v)| {
+                        f.update(*v);
+                    }),
+            )
+        })
+    });
+
+    let mut frac_delay3_f32 = polynomial_fractional_delay::<3, f32>(0.5);
+    c.bench_function("fractional delay order3 f32", |b| {
+        b.iter(|| {
+            let input = black_box(1.0);
+            black_box(frac_delay3_f32.update(input))
+        })
+    });
+
+    let mut frac_delay3_bank = [frac_delay3_f32; 20];
+    c.bench_function("fractional delay order3 f32 bank 20x", |b| {
+        let input = black_box([1.0; 20]);
+        b.iter(|| {
+            black_box(
+                frac_delay3_bank
+                    .iter_mut()
+                    .zip(input.iter())
+                    .for_each(|(f, v)| {
+                        f.update(*v);
+                    }),
+            )
+        })
     });
 }
 
